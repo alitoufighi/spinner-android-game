0e205947be9fb2f0d51fef8509cf0f27c9cd1d8a
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-02-28 10:47:26 +0330

    access gyroscope data

--- app/src/main/java/ir/ac/ut/ece/spinner/GyroscopeActivity.java
+++ app/src/main/java/ir/ac/ut/ece/spinner/GyroscopeActivity.java
@@ -1,13 +1,64 @@
 package ir.ac.ut.ece.spinner;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class GyroscopeActivity extends AppCompatActivity {
+public class GyroscopeActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         setContentView(R.layout.activity_gyroscope);
+        tv = (TextView) findViewById(R.id.tv);
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        /*register the sensor listener to listen to the gyroscope sensor, use the
+        callbacks defined in this class, and gather the sensor information as quick
+        as possible*/
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //When this Activity isn't visible anymore
+    @Override
+    protected void onStop()
+    {
+        //unregister the sensor listener
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1)
+    {
+        //Do nothing.
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        //if sensor is unreliable, return void
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+        {
+            return;
+        }
+
+        //else it will output the Roll, Pitch and Yawn values
+        tv.setText("Orientation X (Roll) :"+ Float.toString(event.values[2]) +"\n"+
+                "Orientation Y (Pitch) :"+ Float.toString(event.values[1]) +"\n"+
+                "Orientation Z (Yaw) :"+ Float.toString(event.values[0]));
     }
 }
