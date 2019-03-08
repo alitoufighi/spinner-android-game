a4bc62b193c90e16c9236c0173766bf567375091
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-03-08 18:58:12 +0330

    apply friction when device is is in fixed position

--- app/src/main/java/ir/ac/ut/ece/spinner/GravityActivity.java
+++ app/src/main/java/ir/ac/ut/ece/spinner/GravityActivity.java
@@ -13,19 +13,55 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
-public class GravityActivity extends AppCompatActivity implements SensorEventListener {
+import java.util.List;
+
+public class GravityActivity
+        extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor sensor;
-    private double ballWeight = 0.01;
     private double samplingRate = 0.04;
-    private float xPos, xAccel, xVel, xAngle, xN = 0.0f;
-    private float yPos, yAccel, yVel, yAngle, yN = 0.0f;
+    private static final double ballWeight = 0.01;
+    private float xPos, xVel, xAccel;
+    private float yPos, yVel, yAccel;
     private float xMax, yMax;
     private ImageView ballImage;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(sensor != null){
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+        } else {
+            System.out.println("--SENSOR IS NULL");
+        }
+    }
+
+    private void getSensorList() {
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder strLog = new StringBuilder();
+        int iIndex = 1;
+        for (Sensor item : sensors) {
+            strLog.append(iIndex).append(".");
+            strLog.append(" Sensor Type - ").append(item.getType()).append("\r\n");
+            strLog.append(" Sensor Name - ").append(item.getName()).append("\r\n");
+            strLog.append(" Sensor Version - ").append(item.getVersion()).append("\r\n");
+            strLog.append(" Sensor Vendor - ").append(item.getVendor()).append("\r\n");
+            strLog.append(" Maximum Range - ").append(item.getMaximumRange()).append("\r\n");
+            strLog.append(" Minimum Delay - ").append(item.getMinDelay()).append("\r\n");
+            strLog.append(" Power - ").append(item.getPower()).append("\r\n");
+            strLog.append(" Resolution - ").append(item.getResolution()).append("\r\n");
+            strLog.append("\r\n");
+            iIndex++;
+        }
+        System.out.println(strLog.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSensorList();
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
@@ -33,22 +69,26 @@ public class GravityActivity extends AppCompatActivity implements SensorEventLis
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        if(sensor == null){
+            System.out.println("NULL SENSOR");
+        }
         setContentView(R.layout.activity_gravity);
         ballImage = findViewById(R.id.ballImage);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        ballImage.setX(size.x / 2);
-        ballImage.setY(size.y / 2);
+        ballImage.setX(size.x / 2f);
+        ballImage.setY(size.y / 2f);
         xMax = (float) size.x - 250;
         yMax = (float) size.y - 250;
     }
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),SensorManager.SENSOR_DELAY_GAME);
-    }
+
+//    @Override
+//    protected void onResume()
+//    {
+//        super.onResume();
+//        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),SensorManager.SENSOR_DELAY_GAME);
+//    }
 
     @Override
     protected void onStop()
@@ -67,35 +107,42 @@ public class GravityActivity extends AppCompatActivity implements SensorEventLis
     public void onSensorChanged(SensorEvent event)
     {
         if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
-        {
             return;
-        }
+        System.out.println("SENSOR CHANGED");
+
         xAccel = event.values[0];
         yAccel = -event.values[1];
         updateBall();
         ballImage.setX(xPos);
         ballImage.setY(yPos);
     }
+
+
     private void updateBall() {
         float frameTime = 0.25f;
         float gravity = 9.81f;
-        xAngle = (float) Math.asin(xAccel / gravity);
-        System.out.println("X ANGLE:" + Float.toString(xAngle));
-        yAngle = (float) Math.asin(yAccel / gravity);
-        System.out.println("Y ANGLE:" + Float.toString(yAngle));
-        xN = (float) (ballWeight * gravity * Math.cos(xAngle));
-        yN = (float) (ballWeight * gravity * Math.cos(yAngle));
+        float xAngle = (float) Math.asin(xAccel / gravity);
+        float yAngle = (float) Math.asin(yAccel / gravity);
+        System.out.println("X ANGLE:" + Float.toString(xAngle) +
+                "X VEL: " + Float.toString(xVel));
+        System.out.println("Y ANGLE:" + Float.toString(yAngle) +
+                "Y VEL: " + Float.toString(yVel));
+        float xN = (float) (ballWeight * gravity * Math.cos(xAngle));
+        float yN = (float) (ballWeight * gravity * Math.cos(yAngle));
+
         if(xVel == 0) {
             if(Math.abs(ballWeight * xAccel) >= Math.abs(xN * 0.15f)) {
                 xVel += (xAccel * frameTime);
                 float xS = (float) (xAccel * Math.pow(frameTime, 2) / 2 + xVel * frameTime);
                 xPos -= xS;
-            } else {
-                xVel = 0;
             }
         } else {
+            if(xVel > 0)
+                xAccel -= ballWeight / (0.1f * xN);
+            else
+                xAccel += ballWeight / (0.1f * xN);
             xVel += (xAccel * frameTime);
-            float xS = (float) ((xAccel - ballWeight / (0.1f * xN))  * Math.pow(frameTime, 2) / 2 + xVel * frameTime);
+            float xS = (float) (xAccel * Math.pow(frameTime, 2) / 2 + xVel * frameTime);
             xPos -= xS;
         }
 
@@ -106,17 +153,20 @@ public class GravityActivity extends AppCompatActivity implements SensorEventLis
             xPos = 0;
             xVel = 0;
         }
+
         if(yVel == 0) {
             if(Math.abs(ballWeight * yAccel) >= Math.abs(yN * 0.15f)) {
                 yVel += (yAccel * frameTime);
                 float yS = (float) (yAccel * Math.pow(frameTime, 2) / 2 + yVel * frameTime);
                 yPos -= yS;
-            } else {
-                yVel = 0;
             }
         } else {
+            if(yVel > 0)
+                yAccel -= ballWeight / (0.1f * yN);
+            else
+                yAccel += ballWeight / (0.1f * yN);
             yVel += (yAccel * frameTime);
-            float yS = (float) ((yAccel - ballWeight / (0.1f * yN))  * Math.pow(frameTime, 2) / 2 + yVel * frameTime);
+            float yS = (float) (yAccel * Math.pow(frameTime, 2) / 2 + yVel * frameTime);
             yPos -= yS;
         }
 
